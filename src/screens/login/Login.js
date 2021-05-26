818b91185d4168efb18f4cc1e21174e12366ef0e
pramehta <pramehta@adobe.com>
2021-05-26 23:54:59 +0530

    Adding Header

---
+++ src/screens/login/Login.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+    constructor(){
+        super();
+     }
+
+    render() {
+    return (
+        <div>
+        </div>
+    );
+    }
+}
+
+export default App;
