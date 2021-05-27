6c952b64db5fb18ab0e08336186289c26dd7e6e5
pramehta <pramehta@adobe.com>
2021-05-27 14:48:38 +0530

    Login UI design

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './common/Header';
+import Login from './screens/login/Login';
 import './common/Header.css';
 
 ReactDOM.render(
     <React.StrictMode>
+        <div>
         <Header />
+        <Login />
+        </div>
     </React.StrictMode>,
     document.getElementById('root')
 );
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,16 +1,46 @@
 import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import { Card, CardContent, Typography } from '@material-ui/core';
+import './Login.css';
 
 class Login extends Component {
-    constructor(){
+    constructor() {
         super();
-     }
+    }
 
     render() {
-    return (
-        <div>
-        </div>
-    );
+        return (
+            <div className="loginFormContainer">
+                <Card className="card">
+                    <CardContent className="card-content">
+                        <Typography variant="h5">LOGIN</Typography>
+                        <br />
+                        <FormControl className="form-control" required>
+                            <InputLabel htmlFor="username">Username</InputLabel>
+                            <Input id="username" type="text" />
+                            <FormHelperText>
+                                <span className="red">required</span>
+                            </FormHelperText>
+                        </FormControl>
+                        <br /><br />
+                        <FormControl className="form-control" required>
+                            <InputLabel htmlFor="loginPassword">Password</InputLabel>
+                            <Input id="loginPassword" type="password" />
+                            <FormHelperText>
+                                <span className="red">required</span>
+                            </FormHelperText>
+                        </FormControl>
+                        <br /><br /><br />
+                        <Button variant="contained" color="primary">LOGIN</Button>
+                    </CardContent>
+                </Card>
+            </div>
+        );
     }
 }
 
-export default App;
+export default Login;
