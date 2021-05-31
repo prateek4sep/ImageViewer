3aaddbe4030f702f15344da00233e94194063e2b
pramehta <pramehta@adobe.com>
2021-05-31 17:47:43 +0530

    Adding Code Comments for all files

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -26,12 +26,13 @@ class Home extends Component {
             searchText: "",
             userImages: [],
             filteredImages: [],
-            username: properties.username,
-            url: properties.dpUrl,
+            username: properties.username, // Reading hard-coded username from config file.
+            url: properties.dpUrl, // Reading hard-coded profile picture URL from config file.
             loggedIn: sessionStorage.getItem("access-token") == null ? false : true
         }
     }
 
+    // Hook that gets invoked right after a React component has been mounted aka after the first render() lifecycle.
     async componentDidMount() {
             let getUserImages = this.props.baseUrl + "me/media?fields=id,caption&access_token=" + sessionStorage.getItem("access-token");
             let getPostDetails = this.props.baseUrl + "$postId" + "?fields=id,media_type,media_url,username,timestamp&access_token=" + sessionStorage.getItem("access-token");
@@ -47,16 +48,17 @@ class Home extends Component {
                 posts[i].url = details.media_url;
                 posts[i].username = details.username;
                 posts[i].timestamp = details.timestamp;
-                posts[i].comments = [];
-                posts[i].tags = "#upgrad #upgradproject #reactjs";
-                posts[i].likes = Math.round(Math.random() * 100);
-                posts[i].isLiked = false;
+                posts[i].comments = []; // For adding new comments.
+                posts[i].tags = properties.hashTags; // Reading hard-coded hashtags from config file.
+                posts[i].likes = Math.round(Math.random() * 100); // Random likes for each image.
+                posts[i].isLiked = false; // Setting liked status for the current user.
             }
             this.setState({ userImages: posts });
-            this.setState({ filteredImages: posts.filter(x => true) });
+            this.setState({ filteredImages: posts.filter(x => true) }); // Filtering images for search query.
     }
 
 
+    // On clicking like button. Setting like for the logged in user.
     likeHandler = (details) => {
         let index = details.index;
         let likedImages = this.state.userImages;
@@ -64,6 +66,7 @@ class Home extends Component {
         this.setState({'userImages': likedImages})
     }
 
+    // On clicking Add button. Adding newly added comment for the corresponding image.
     commentHandler = (details, pos) => {
         let index = details.index;
         var textField = document.getElementById("textfield-" + pos);
@@ -82,6 +85,7 @@ class Home extends Component {
         this.setState({'userImages': userImagesTemp})
     }
 
+    // Filtering the images based on the entered text.
     searchHandler = (e) => {
         if (this.state.searchText == null || this.state.searchText.trim() === "") {
             this.setState({filteredImages: this.state.userImages});
@@ -93,6 +97,7 @@ class Home extends Component {
         }
     }
 
+    // For syncing a change in state of the entered search text.
     handleChange = (e) => {
         this.setState({'searchText': e.target.value}, () => {
             this.searchHandler(e);
@@ -100,19 +105,25 @@ class Home extends Component {
     };
 
     render() {
+        // Redirect to login page if not logged in.
         if(this.state.loggedIn===false) return <Redirect to="/" />
         else
         return (
             <div>
                 <div>
+                    {/* Invoking Header component with Show Search and Show My Account flags. */}
                     <Header {...this.props} loggedIn={true} dpUrl={this.state.url} showMyAccount={true} showSearch={true} handleChange={this.handleChange} />
                     </div>
+
+                    {/* Posts Master Container. */}
                     <Container className='posts-container'>
                     <Grid container alignContent='center' justify='flex-start' direction='row' spacing={2}>
                         {
                             (this.state.filteredImages || []).map((details, index) => (
                                 <Grid item xs={6} key={details.id+"_img"}>
                                     <Card key={details.id}>
+
+                                        {/* Card Header, Media and Content. */}
                                         <CardHeader
                                             avatar={<Avatar variant="circle" src={this.state.url} className='avatar' />}
                                             title={details.username}
@@ -135,6 +146,7 @@ class Home extends Component {
                                                 </Typography>
                                             </div>
 
+                                            {/* Displaying available comments for corresponding image. */}
                                             <div id='comments-container'>
                                                 {
                                                     details.comments ?
@@ -148,6 +160,7 @@ class Home extends Component {
                                                 }
                                             </div>
 
+                                            {/* Form for adding comments to an image. */}
                                             <div className='post-comment-container'>
                                                 <FormControl className='post-comment-form-control'>
                                                     <TextField id={'textfield-' + index} label="Add a comment" />
